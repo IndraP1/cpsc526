3479ec1dfa833a67d312c9faeca1ef37122407ed
indra prasetyo <indra.prasetyo@calgaryscientific.com>
2017-02-12 20:49:46 -0700

    Create argparser

--- assignment2/proxy.py
+++ assignment2/proxy.py
@@ -1,41 +1,51 @@
 #!/usr/bin/python
-import sys
 import socketserver
-import threading
+import socket
+from threading import Thread
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--src_port', help='', required=True)
+parser.add_argument('--dst_port', help='', required=True)
+parser.add_argument('--server', help='', required=True)
+parser.add_argument('--raw', help='', action='store_true', required=False)
+parser.add_argument('--strip', help='', action='store_true', required=False)
+parser.add_argument('--hex', help='', action='store_true', required=False)
+parser.add_argument('--auto', action='store', metavar='N', help='')
+args = parser.parse_args()
+
+class MyTCPConnection(Thread):
+    def __init__(self, proxy_source):
+        self.proxy_source = proxy_source
+        self.proxy_target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        self.proxy_target.connect()
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     BUFFER_SIZE = 4096
 
-def handle(self):
-    try:
-        while True:
-            msg = self.request.recv(self.BUFFER_SIZE)
-    except IOError as e:
-        print("Error occured {}".format(str(e)))
+    def handle(self):
+        try:
+            while True:
+                # msg = self.request.recv(self.BUFFER_SIZE)
+        except IOError as e:
+            print("Error occured {}".format(str(e)))
+
+    # def forward_requests():
 
 if __name__ == "__main__":
     print("Starting proxy server")
-
-    if len(sys.argv) == 5:
-        log_level = sys.argv[1]
-        src_port = int(sys.argv[2])
-        server = sys.argv[3]
-        dst_port = int(sys.argv[4])
-    elif len(sys.argc) == 4:
-        log_level = 'default'
-        src_port = int(sys.argv[1])
-        server = sys.argv[2]
-        dst_port = int(sys.argv[3])
-
-    server = socketserver.ThreadingTCPServer((server, src_port), MyTCPHandler)
+    server = socketserver.ThreadingTCPServer((args.server, args.src_port), MyTCPHandler)
     # thread = threading.Thread(target=server.serve_forever)
-    thread = threading.Thread(target=server.serve_forever).start()
+    thread = Thread(target=server.serve_forever).start()
     # thread.daemon = True
     # thread.start()
 
-    print("Server taking requests on port " + src_port)
-    print("Server forwarding requests to port " + src_port + " of " + server)
+    print("Server taking requests on port " + args.src_port)
+    print("Server forwarding requests to port " + args.src_port + " of " +
+          args.server)
 
     while True:
         time.sleep(1)
