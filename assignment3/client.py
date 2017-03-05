90a51cb6a7d43078d6916528d4a45978194866ff
indra prasetyo <indra.prasetyo@calgaryscientific.com>
2017-03-05 15:14:27 -0700

    READ command and communication working

--- assignment3/client.py
+++ assignment3/client.py
@@ -1,14 +1,70 @@
 #!/usr/bin/python
 import argparse
+import socket
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--command', type=str, help='', required=True)
 parser.add_argument('--filename', type=str, help='', required=True)
 parser.add_argument('--hostname', type=str, help='', required=True)
 parser.add_argument('--port', type=int, help='', required=True)
-parser.add_argument('--cipher', type=str, help='', required=True)
+# parser.add_argument('--cipher', type=str, help='', required=False) #True
 parser.add_argument('--key', type=str, help='', required=False)
 args = parser.parse_args()
 
+
+class MyTCPConnection():
+    BUFFER = 4096
+
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((args.hostname, args.port))
+
+    def run(self):
+        try:
+            # establish connection
+            # send request
+            self.generate_request()
+            while True:
+                msg = self.receive()
+                if len(msg) == 0:
+                    self.stop()
+                elif msg == 'OK':
+                    print("ok")
+                    self.stop()
+                else:
+                    # decrypt message
+                    print(msg)
+        except Exception as e:
+            print("Error occured {}".format(str(e)))
+
+    def generate_request(self):
+        if (args.command == "write"):
+            print("payload")
+            # TODO send small amounts of data at a time
+            payload = sys.stdin.read()
+            print(payload)
+            command = "WRITE " + args.filename
+        elif (args.command == "read"):
+            command = "READ " + args.filename
+        else:
+            print("ERROR: Invalid command")
+            exit()
+
+        self.send(command)
+
+    def send(self, msg):
+        self.client_socket.sendall(bytes(msg + '\n', 'utf-8'))
+
+    def receive(self):
+        msg = self.client_socket.recv(self.BUFFER).decode('utf-8').rstrip('\n')
+        return msg
+
+    def stop(self):
+        self.client_socket.close()
+        exit()
+
 if __name__ == "__main__":
     print(args)
+    connection = MyTCPConnection()
+    connection.run()
--- assignment3/server.py
+++ assignment3/server.py
@@ -2,39 +2,60 @@
 import argparse
 import socketserver
 
-OK = 'OK\n'
+OK = 'OK'
+DONE = 'done\n'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', type=int, help='', required=True)
 parser.add_argument('--key', type=str, help='', required=False)
 args = parser.parse_args()
 
+
 class TCPHandler(socketserver.BaseRequestHandler):
-    BUFFER_SIZE = 4096
+    BUFFER = 4096
+
     def handle(self):
         try:
-            # Probably try to confirm secret yhere
+            # Probably try to confirm secret here
             # self.authenticate if possible
+            print("new client: " + self.client_address[0] + " crypto: NONE")
             while True:
-                command = self.receive()
-                output = self.execute_command(command)
-                if output == 'invalid':
-                    self.send_n("error")
-                else:
-                    self.send_nl(OK)
+                msg = self.receive()
+                print("DEBUG:" + msg)
+                command = str.split(msg)
+                self.execute_command(command[0], command[1])
+                # command = self.receive()
+                # output = self.execute_command(command)
+                break
+            print(DONE)
 
         except IOError as e:
             print("Error occured {}".format(str(e)))
 
     def receive(self):
-        msg = self.request.recv(1024).decode('utf-8').rstrip('\n')
+        msg = self.request.recv(self.BUFFER).decode('utf-8').rstrip('\n')
         return msg
 
-    def execute_command(self, command):
-        print("test")
+    def execute_command(self, command, filename):
+        if (command == "READ"):
+            try:
+                with open(filename) as f:
+                    for line in f:
+                        self.send(line)
+                self.send(OK)
+            except Exception as e:
+                self.send_nl("ERROR: File " + filename + " does not exist")
+                print("Error occured {}".format(str(e)))
+
+        elif (command == "WRITE"):
+            # TODO
+            print("WIP Write")
 
     def send_nl(self, msg):
         self.request.sendall(bytes(msg + '\n', 'utf-8'))
+
+    def send(self, msg):
+        self.request.sendall(bytes(msg, 'utf-8'))
 
 if __name__ == "__main__":
     print("Listening on port " + str(args.port))
